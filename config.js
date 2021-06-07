d0cb96f6bf8631ce82a3034674f6e8428fb82a5b
sarahimran <sarahimran1024@gmail.com>
2021-06-07 22:52:17 +0500

    First commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://192.168.43.72:5000/api';
+const API_URL = 'http://192.168.1.8:5000/api';
 const extra1 = 'http://192.168.43.72:5000/api';
 const extra2 = 'http://10.0.0.3:5000/api';
 export default API_URL;
